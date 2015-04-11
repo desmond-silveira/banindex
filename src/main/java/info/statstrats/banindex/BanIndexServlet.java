0c4492812d1ad14dd18a3ceb63bf1d0d16986a7a
Desmond Silveira <dsilveira@local.com>
2015-04-10 17:44:19 -0700

    Initial

---
+++ src/main/java/info/statstrats/banindex/BanIndexServlet.java
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) 2015 Local Corporation. All Rights Reserved.
+ */
+package info.statstrats.banindex;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * @author dsilveira
+ */
+@WebServlet(urlPatterns = "/")
+public class BanIndexServlet extends HttpServlet {
+
+    private static final long serialVersionUID = -1328271289169533523L;
+
+}
---
+++ src/main/java/info/statstrats/banindex/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Copyright (c) 2015 Local Corporation. All Rights Reserved.
+ */
+/**
+ * @author dsilveira
+ */
+package info.statstrats.banindex;
