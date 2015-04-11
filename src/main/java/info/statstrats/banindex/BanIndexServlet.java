53c0c8e9db77e030c64ecca884d13d138dab62d2
Desmond Silveira <dsilveira@local.com>
2015-04-10 18:10:35 -0700

    Removed erroneous file headers.

--- src/main/java/info/statstrats/banindex/BanIndexServlet.java
+++ src/main/java/info/statstrats/banindex/BanIndexServlet.java
@@ -1,6 +1,3 @@
-/**
- * Copyright (c) 2015 Local Corporation. All Rights Reserved.
- */
 package info.statstrats.banindex;
 
 import javax.servlet.annotation.WebServlet;
--- src/main/java/info/statstrats/banindex/package-info.java
+++ src/main/java/info/statstrats/banindex/package-info.java
@@ -1,7 +1,4 @@
 /**
- * Copyright (c) 2015 Local Corporation. All Rights Reserved.
- */
-/**
  * @author dsilveira
  */
 package info.statstrats.banindex;
